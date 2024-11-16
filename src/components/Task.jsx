2055eb1e6f3ace7b93df6ec7db798cc0dd9d890e
Michael Trang <michael.trang@hotmail.com>
2024-11-16 03:17:06 +0000

    Handle long task title

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,40 +1,42 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-export default function Task({ task: { id, title, state }, onArchiveTask, onPinTask }) {
+export default function Task({
+  task: { id, title, state },
+  onArchiveTask,
+  onPinTask,
+}) {
   return (
     <div className={`list-item ${state}`}>
       <label
         htmlFor={`archiveTask-${id}`}
         aria-label={`archiveTask-${id}`}
-        className="checkbox"
+        className='checkbox'
       >
         <input
-          type="checkbox"
+          type='checkbox'
           disabled={true}
-          name="checked"
+          name='checked'
           id={`archiveTask-${id}`}
           checked={state === "TASK_ARCHIVED"}
         />
-        <span
-          className="checkbox-custom"
-          onClick={() => onArchiveTask(id)}
-        />
+        <span className='checkbox-custom' onClick={() => onArchiveTask(id)} />
       </label>
 
-      <label htmlFor={`title-${id}`} aria-label={title} className="title">
+      <label htmlFor={`title-${id}`} aria-label={title} className='title'>
         <input
-          type="text"
+          type='text'
           value={title}
           readOnly={true}
-          name="title"
+          name='title'
           id={`title-${id}`}
-          placeholder="Input title"
+          placeholder='Input title'
+          style={{ textOverflow: "ellipsis" }}
         />
       </label>
 
       {state !== "TASK_ARCHIVED" && (
         <button
-          className="pin-button"
+          className='pin-button'
           onClick={() => onPinTask(id)}
           id={`pinTask-${id}`}
           aria-label={`pinTask-${id}`}
@@ -48,17 +50,17 @@ export default function Task({ task: { id, title, state }, onArchiveTask, onPinT
 }
 
 Task.propTypes = {
-	/** Composition of the task */
-	task: PropTypes.shape({
-	/** Id of the task */
-	id: PropTypes.string.isRequired,
-	/** Title of the task */
-	title: PropTypes.string.isRequired,
-	/** Current state of the task */
-	state: PropTypes.string.isRequired,
-	}),
-	/** Event to change the task to archived */
-	onArchiveTask: PropTypes.func,
-	/** Event to change the task to pinned */
-	onPinTask: PropTypes.func,
+  /** Composition of the task */
+  task: PropTypes.shape({
+    /** Id of the task */
+    id: PropTypes.string.isRequired,
+    /** Title of the task */
+    title: PropTypes.string.isRequired,
+    /** Current state of the task */
+    state: PropTypes.string.isRequired,
+  }),
+  /** Event to change the task to archived */
+  onArchiveTask: PropTypes.func,
+  /** Event to change the task to pinned */
+  onPinTask: PropTypes.func,
 };
--- src/components/Task.stories.jsx
+++ src/components/Task.stories.jsx
@@ -1,49 +1,60 @@
-import { fn } from "@storybook/test"
+import { fn } from "@storybook/test";
 
-import Task from "./Task"
+import Task from "./Task";
 
 export const ActionsData = {
-	onArchiveTask: fn(),
-	onPinTask: fn(),
+  onArchiveTask: fn(),
+  onPinTask: fn(),
 };
 
 const meta = {
-	component: Task,
-	title: 'Task',
-	tags: ['autodocs'],
-	// Our exports that end in "Data" are not stories
-	excludeStories: /.*Data$/,
-	args: {
-		...ActionsData,
-	}
-}
+  component: Task,
+  title: "Task",
+  tags: ["autodocs"],
+  // Our exports that end in "Data" are not stories
+  excludeStories: /.*Data$/,
+  args: {
+    ...ActionsData,
+  },
+};
 
 export default meta;
 
 export const Default = {
-	args: {
-		task: {
-			id: '1',
-			title: 'Test Task',
-			state: 'TASK_INBOX',
-		}
-	}
-}
+  args: {
+    task: {
+      id: "1",
+      title: "Test Task",
+      state: "TASK_INBOX",
+    },
+  },
+};
 
 export const Pinned = {
-	args: {
-		task: {
-			...Default.args.task,
-			state: 'TASK_PINNED',
-		}
-	}
-}
+  args: {
+    task: {
+      ...Default.args.task,
+      state: "TASK_PINNED",
+    },
+  },
+};
 
 export const Archived = {
-	args: {
-		task: {
-			...Default.args.task,
-			state: 'TASK_ARCHIVED',
-		}
-	}
-}
+  args: {
+    task: {
+      ...Default.args.task,
+      state: "TASK_ARCHIVED",
+    },
+  },
+};
+
+const longTitleString = `This task's name is absurdly large. In fact, I think if I keep going I might end up with content overflow. What will happen? The star that represents a pinned task could have text overlapping. The text could cut-off abruptly when it reaches the star. I hope not!`;
+
+export const LongTitle = {
+  args: {
+    task: {
+      ...Default.args.task,
+      title: longTitleString,
+    },
+  },
+};
